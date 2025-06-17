815a7a3278fc5c20455ce4e4601d4c5e275267e1
Donato Fiore <donato.v.fiore@gmail.com>
2025-06-16 22:16:27 -0400

    Update localization filename

--- CalculateUnits/CalculateUnit.m
+++ CalculateUnits/CalculateUnit.m
@@ -22,7 +22,7 @@
             _displayName = [locale displayNameForKey:NSLocaleCurrencyCode value:_name];
         } else {
             NSString *title = [_name stringByAppendingString:@" (Title)"];
-            _displayName = [[NSBundle mainBundle] calc_localizedStringForKey:title value:@"Error" table:@"LocalizableUnits"];
+            _displayName = [[NSBundle mainBundle] calc_localizedStringForKey:title value:_name table:@"LocalizableUnits"];
         }
     }
 
@@ -32,7 +32,7 @@
 - (NSString *)shortName {
     if (!_shortName) {
         NSString *title = [_name stringByAppendingString:@" (Short)"];
-        _shortName = [[NSBundle mainBundle] calc_localizedStringForKey:title value:@"Error" table:@"LocalizableUnits"];
+        _shortName = [[NSBundle mainBundle] calc_localizedStringForKey:title value:_name table:@"LocalizableUnits"];
     }
 
     return _shortName;
