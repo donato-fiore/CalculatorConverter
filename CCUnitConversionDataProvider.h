82c144adeb81876b4787db8f09f0974dc4db97ec
Donato Fiore <donato.v.fiore@gmail.com>
2025-06-16 22:41:35 -0400

    Add a shared reference for unit collection

--- CCUnitConversionDataProvider.h
+++ CCUnitConversionDataProvider.h
@@ -1,9 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <Calculate/Converter.h>
+#import "CalculateUnits/CalculateUnitCollection.h"
 
 @interface CCUnitConversionDataProvider : NSObject {
     Converter *_converter;
 }
+@property (nonatomic, strong) CalculateUnitCollection *unitCollection;
+@property (nonatomic, strong) NSBundle *calculateFrameworkBundle;
 @property (nonatomic, assign) NSInteger inputUnitID;
 @property (nonatomic, assign) NSInteger resultUnitID;
++ (instancetype)sharedInstance;
 @end
--- CCUnitConversionDataProvider.m
+++ CCUnitConversionDataProvider.m
@@ -2,11 +2,34 @@
 
 @implementation CCUnitConversionDataProvider
 
++ (instancetype)sharedInstance {
+    static CCUnitConversionDataProvider *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
 - (instancetype)init {
     self = [super init];
 
     if (self) {
-        
+        self.calculateFrameworkBundle = [NSBundle bundleWithPath:@"/System/Library/PrivateFrameworks/Calculate.framework"];
+        if (!self.calculateFrameworkBundle) {
+            NSLog(@"[UnitConversionDataProvider] Could not load Calculate framework bundle.");
+            return nil;
+        }
+        [self.calculateFrameworkBundle load];
+
+        NSString *path = [_calculateFrameworkBundle pathForResource:@"ConverterUnits" ofType:@"plist"];
+        if (!path) {
+            NSLog(@"[UnitConversionDataProvider] Could not find ConverterUnits.plist in bundle %@", _calculateFrameworkBundle.bundlePath);
+            return nil;
+        }
+        NSDictionary *unitsDictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+        self.unitCollection = [[CalculateUnitCollection alloc] init];
+        [self.unitCollection loadCategoriesFromDictionary:unitsDictionary];
     }
 
     return self;
