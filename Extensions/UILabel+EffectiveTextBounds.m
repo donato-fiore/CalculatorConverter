573a802af7f6d45bec17370eab04b181ea0b9c45
Donato Fiore <donato.v.fiore@gmail.com>
2025-06-25 19:47:56 -0400

    fix. effective text bounds being relative to superview

--- Extensions/UILabel+EffectiveTextBounds.m
+++ Extensions/UILabel+EffectiveTextBounds.m
@@ -4,22 +4,27 @@
 
 - (CGRect)effectiveTextBounds {
     NSString *text = self.text;
-    if (text.length == 0) {
-        return CGRectZero;
+    if (text.length == 0) return CGRectZero;
+
+    NSDictionary *attributes = @{ NSFontAttributeName: self.font };
+    CGSize textSize = [text sizeWithAttributes:attributes];
+    CGRect bounds = self.bounds;
+    
+    CGFloat originX = 0;
+    switch (self.textAlignment) {
+        case NSTextAlignmentCenter:
+            originX = (bounds.size.width - textSize.width) / 2;
+            break;
+        case NSTextAlignmentRight:
+            originX = bounds.size.width - textSize.width;
+            break;
+        case NSTextAlignmentLeft:
+        default:
+            originX = 0;
+            break;
     }
 
-    NSDictionary *attributes = @{NSFontAttributeName: self.font};
-    CGSize textSize = [text sizeWithAttributes:attributes];
-    CGRect bounds = CGRectMake(0, 0, textSize.width, textSize.height);
-    CGRect adjustedBounds = CGRectZero;
-    if (self.textAlignment == NSTextAlignmentLeft) {
-        adjustedBounds = CGRectMake(self.frame.origin.x, self.frame.origin.y, bounds.size.width, bounds.size.height);
-    } else if (self.textAlignment == NSTextAlignmentCenter) {
-        adjustedBounds = CGRectMake(self.frame.origin.x + (self.frame.size.width - bounds.size.width) / 2, self.frame.origin.y, bounds.size.width, bounds.size.height);
-    } else if (self.textAlignment == NSTextAlignmentRight) {
-        adjustedBounds = CGRectMake(self.frame.origin.x + self.frame.size.width - bounds.size.width, self.frame.origin.y, bounds.size.width, bounds.size.height);
-    }
-    return adjustedBounds;
+    return CGRectMake(originX, (bounds.size.height - textSize.height) / 2, textSize.width, textSize.height);
 }
 
 @end
